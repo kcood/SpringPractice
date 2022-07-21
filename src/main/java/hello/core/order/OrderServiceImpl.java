c5aa64ca979354f3dabdf37b50213c5952b78f4c
DYS <kcood92@gmail.com>
2022-07-21 14:24:52 +0900

    -Stick to DIP through "APPConfig"

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,25 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    //MemberRepository에서 회원 찾아야되니까
-   private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-   private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+   //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+   //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //얘들은 DIP 위반. 추상인 인터페이스에만 의존하는게 아니라 구체 클래스 (Fix, Rate DiscountPolicy)에 의존
+    private final DiscountPolicy discountPolicy;
+    //MemberRepository에서 회원 찾아야되니까
+    private final MemberRepository memberRepository;
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
